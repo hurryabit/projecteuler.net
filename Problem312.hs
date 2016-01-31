cd9316cbd3ab04683eeac974d2c706c5a2c5e304
Martin Huschenbett <martin.huschenbett@posteo.de>
2016-01-31 18:40:38 +0100

    Dressed up solution to problem 312 for forum.

--- Problem312.hs
+++ Problem312.hs
@@ -1,75 +1,61 @@
-import Data.Bits
-import Prelude hiding ((^^))
-import Numbers (factorization, chinese2, invMod, powMod, divMaxPow, divides)
+import Data.List (group)
+import Data.MemoCombinators (bits)
+import Data.Numbers.Primes (primeFactors)
 
-solution = eval (c $ c $ c $ 10000) (13^8)
+main = print $ evalModRep (c $ c $ c $ 10000) (13^8)
 
-c :: Mod -> Mod
-c m = 2 ^^ (3 ^^ (m-2)) * 3 ^^ (3 * ((3 ^^ (m-3) - 1) // 2))
+c :: ModRep -> ModRep
+c m = 2 ^. (3 ^. (m-2)) * 3 ^. (3 * ((3 ^. (m-3) - 1) /. 2))
 
-newtype Nat = Nat [(Int,Int)]
-  deriving (Show, Eq)
+-- represents a number x by the map \n -> x `mod` n
+newtype ModRep = ModRep { evalModRep :: Integer -> Integer }
 
-prime :: Int -> Nat
-prime p = Nat [(p,1)]
+-- create a representation which uses memoization
+mkModRep :: (Integer -> Integer) -> ModRep
+mkModRep = ModRep . bits
 
-primePower :: Int -> Int -> Nat
-primePower p k = Nat [(p,k)]
+instance Num ModRep where
+  fromInteger a = ModRep $ mod a
+  ModRep f + ModRep g = mkModRep $ \n -> (f n + g n) `mod` n
+  ModRep f * ModRep g = mkModRep $ \n -> (f n * g n) `mod` n
+  ModRep f - ModRep g = mkModRep $ \n -> (f n - g n) `mod` n
 
-fromInt :: Int -> Nat
-fromInt n
-  | n > 0     = Nat (factorization n)
-  | otherwise = error $ show n ++ " is not a natural number"
+-- factorization
+factorize :: Integer -> [(Integer,Integer)]
+factorize = map ((,) <$> head <*> (toInteger . length)) . group . primeFactors
 
-toInt :: Nat -> Int
-toInt (Nat pks) = product $ map (uncurry (^)) pks
+-- Euler's totient function
+phi :: Integer -> Integer
+phi n = product [ (p-1) * p^(k-1) | (p,k) <- factorize n ]
 
-instance Num Nat where
-  fromInteger = fromInt . fromInteger
-  (+) = error $ "+ undefined for Nat"
-  (-) = error $ "- undefined for Nat"
-  Nat pks * Nat qls = Nat (pks `merge` qls)
-    where
-      pks         `merge` []          = pks
-      []          `merge` qls         = qls
-      ((p,k):pks) `merge` ((q,l):qls) = case p `compare` q of
-        LT -> (p,k)   : (pks         `merge` ((q,l):qls))
-        EQ -> (p,k+l) : (pks         `merge` qls        )
-        GT -> (q,l)   : (((p,k):pks) `merge` qls        )
-  negate = error $ "negate undefined for Nat"
-  abs = error $ "abs undefined for Nat"
-  signum = error $ "signum undefined for Nat"
+-- Euclid's extended algorithm
+euclid :: Integer -> Integer -> (Integer,Integer,Integer)
+euclid a b
+  | c == 0    = (b,0,1)
+  | otherwise = let (d,r,s) = euclid b c
+                in  (d,s,r-s*q)
+  where (q,c) = a `divMod` b
 
-phi :: Nat -> Nat
-phi (Nat pks) =
-  let pks' = [ (p,k-1) | (p,k) <- pks, k > 1 ]
-  in  Nat pks' * product (map (fromInt . pred . fst) pks)
+-- Chinese remainder theorem applied
+chinese :: (Integer -> Integer -> Integer) -> ModRep
+chinese base = mkModRep $ \n -> fst $ foldl combine (0,1) [ (base p k,p^k) | (p,k) <- factorize n ]
+  where
+    combine (a,m) (b,n) =
+      let (1,r,s) = euclid m n
+      in  ((s*n*a+r*m*b) `mod` (m*n),m*n)
+    
+-- division by a constant
+(/.) :: ModRep -> Integer -> ModRep
+ModRep f /. b = chinese $ \p k ->
+  let n = p^k
+      -- split b into m*c such that m is power of p and c is no more divisible by p
+      (m,c):_ = dropWhile (\(_,y) -> y `mod` p == 0) $ iterate (\(x,y) -> (p*x,y `div` p)) (1,b)
+      -- invert c modulo n
+      (1,c',_) = euclid (c `mod` n) n      
+  in  (f (n * m) `div` m * c' `mod` n) `mod` n
 
-(%) :: Int -> Nat -> Int
-a % n = a `mod` toInt n
-
-
-newtype Mod = Mod { eval :: Nat -> Int }
-
-instance Num Mod where
-  fromInteger a = Mod $ \n -> fromInteger a % n
-  Mod f + Mod g = Mod $ \n -> (f n + g n) % n
-  Mod f * Mod g = Mod $ \n -> (f n * g n) % n
-  Mod f - Mod g = Mod $ \n -> (f n - g n) % n
-  abs = error $ "abs undefined for Mod"
-  signum = error $ "signum undefined for Mod"
-
-chinese :: (Int -> Int -> Int) -> Mod
-chinese base = Mod $ \(Nat pks) -> fst $ foldl chinese2 (0,1) [ (base p k ,p^k) | (p,k) <- pks ]
-
-(//) :: Mod -> Int -> Mod
-Mod f // r = chinese $ \p k ->
-  let (s,l) = divMaxPow r p
-      p'k = p^k
-  in  (f (primePower p (k+l)) `div` (p^l) * invMod s p'k) `mod` p'k
-
-(^^) :: Mod -> Mod -> Mod
-Mod f ^^ Mod g = chinese $ \p k ->
-  let n   = primePower p k
-      a   = f n
-  in  if p `divides` a then 0 else powMod a (g (phi n)) (p^k)
+-- fast exponentiation using Euler's theorem
+(^.) :: ModRep -> ModRep -> ModRep
+a@(ModRep f) ^. ModRep g = chinese $ \p k ->
+  let n   = p^k
+  in  if f n `mod` p == 0 then 0 else evalModRep (a ^ g (phi n)) n
