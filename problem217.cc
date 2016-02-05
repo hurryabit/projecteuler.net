cfde72a0b21d6a708cdffd7ae9cba81d954b1e52
Martin Huschenbett <martin.huschenbett@posteo.de>
2016-02-05 22:51:13 +0100

    Made C++ solution to problem 217 more space efficient.

--- problem217.cc
+++ problem217.cc
@@ -78,26 +78,36 @@ typedef mod_t<nat_t, 14348907> sum_t;
 
 int main () {
   const nat_t K = 23;
-  multiset_t<sum_t> xs[K+1][K*9+1];
-  multiset_t<sum_t> ys[K+1][K*9+1];
+  multiset_t<sum_t> xs[K*9+1], xs_old[K*9+1];
+  multiset_t<sum_t> ys[K*9+1], ys_old[K*9+1];
 
-  for( nat_t d = 1; d <= 9; ++d ) {
-    xs[1][d] = singleton<sum_t>(d);
-    ys[1][d] = singleton<sum_t>(d);
-  }
-  ys[0][0] = singleton<sum_t>(0);
-  
   multiset_t<sum_t> t = {9, 45}, digits = {10, 45};
   sum_t ten2k = 10;
   for( nat_t k = 1; k <= K; ++k ) { // num of digits
-    ys[k][0] = singleton<sum_t>(0);
-    for( nat_t d = 1; d <= k*9; ++d ) { // sum of digits
-      for( nat_t l = 0; l <= 9 && l <= d; ++l ) { // new last digit
-        xs[k][d] |= sum_t(10) * xs[k-1][d-l] + singleton<sum_t>(l);
+    if( k == 1 ) {
+      xs[0] = multiset_t<sum_t>();
+      ys[0] = singleton<sum_t>(0);
+      for( nat_t d = 1; d <= 9; ++d ) {
+        xs[d] = singleton<sum_t>(d);
+        ys[d] = singleton<sum_t>(d);
       }
-      ys[k][d] = ys[k-1][d] | xs[k][d];
-      t |= ten2k * xs[k][d] + ys[k][d];
-      t |= ten2k*(sum_t(10) * xs[k][d] + digits) + ys[k][d];
+    }
+    else {
+      swap(xs, xs_old);
+      swap(ys, ys_old);
+      xs[0] = multiset_t<sum_t>();
+      ys[0] = singleton<sum_t>(0);
+      for( nat_t d = 1; d <= k*9; ++d ) { // sum of digits
+        xs[d] = multiset_t<sum_t>();
+        for( nat_t l = 0; l <= 9 && l <= d; ++l ) // new last digit
+          xs[d] |= sum_t(10) * xs_old[d-l] + singleton<sum_t>(l);
+        ys[d] = ys_old[d] | xs[d];
+      }
+    }
+
+    for( nat_t d = 1; d <= k*9; ++d ) { // sum of digits
+      t |= ten2k * xs[d] + ys[d];
+      t |= ten2k*(sum_t(10) * xs[d] + digits) + ys[d];
     }
     ten2k *= 10;
   }
